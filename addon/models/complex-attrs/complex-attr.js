cbca6ae7d7bada05311710054fbd64e1486ee101
Kenneth Buck <kenneth@nestedsphere.com>
2018-02-13 18:31:29 -0500

    Rename variable

--- addon/models/complex-attrs/complex-attr.js
+++ addon/models/complex-attrs/complex-attr.js
@@ -2,10 +2,10 @@ import EmberObject from '@ember/object';
 import {uuid} from 'ember-cli-uuid';
 import attr from 'ember-data-complex-attrs/attr'
 
-const BaseComplexAttr = EmberObject.extend({
+const ComplexAttr = EmberObject.extend({
   id: attr('string', {
     defaultValue: () => uuid()
   })
 });
 
-export default BaseComplexAttr;
+export default ComplexAttr;
