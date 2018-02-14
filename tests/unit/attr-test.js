1b36cc3b5df354a5e94e0f06a1f5c99c24fa25d5
Kenneth Buck <kenneth@nestedsphere.com>
2018-02-13 19:11:41 -0500

    Add basic tests for attr

---
+++ tests/unit/attr-test.js
@@ -0,0 +1,31 @@
+import {module, test} from 'ember-qunit';
+import attr from 'ember-data-complex-attrs/attr';
+import ComplexAttr from 'ember-data-complex-attrs/models/complex-attrs/complex-attr';
+
+module('Unit | attr');
+
+test('can specify default value', function (assert) {
+  assert.expect(1);
+
+  const FooComplexAttr = ComplexAttr.extend({
+    bar: attr('string', {defaultValue: 'baz'})
+  });
+
+  const foo = FooComplexAttr.create();
+
+  assert.strictEqual(foo.get('bar'), 'baz');
+});
+
+test('can get/set value', function (assert) {
+  assert.expect(1);
+
+  const FooComplexAttr = ComplexAttr.extend({
+    bar: attr('string', {defaultValue: 'baz'})
+  });
+
+  const foo = FooComplexAttr.create();
+
+  foo.set('bar', 'baz');
+
+  assert.strictEqual(foo.get('bar'), 'baz');
+});
